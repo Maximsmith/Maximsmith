a158cfc170c323af7ad90fd341bc71a475d2392e
Maxim Smith <maximsmith@gmail.com>
2019-02-13 13:42:22 -0500

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -11,10 +11,5 @@ window.addEventListener('resize', () => {
 });
 
 $(function() {
-  var pgurl = window.location.href.substr(window.location.href
-     .lastIndexOf("/") + 1);
-  $("nav a").each(function() {
-     if ($(this).attr("href") == pgurl || $(this).attr("href") == '')
-        $(this).addClass("active");
-  })
+  $('nav a[href^="/' + location.pathname.split("/")[1] + '"]').addClass('active');
 });
