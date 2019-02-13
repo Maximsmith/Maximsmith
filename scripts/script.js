a3c4c942b57d434800a46ed8e8e56593b7f8d03c
Maxim Smith <maximsmith@gmail.com>
2019-02-13 12:14:17 -0500

    head

--- scripts/script.js
+++ scripts/script.js
@@ -9,14 +9,3 @@ window.addEventListener('resize', () => {
   let vh = window.innerHeight * 0.01;
   document.documentElement.style.setProperty('--vh', `${vh}px`);
 });
-
-$(function(){
-    var current = location.pathname;
-    $('.nav li a').each(function(){
-        var $this = $(this);
-        // if the current path is like this link, make it active
-        if($this.attr('href').indexOf(current) !== -1){
-            $this.addClass('active');
-        }
-    })
-});
